67c42104e3ca1ef79f34c56e15394d4921163427
Blend Mehmeti <blendimehmeti1@gmail.com>
2021-10-29 15:52:51 +0200

    Delete project

--- app/src/main/java/com/deliveryhero/challenges/vendor/android/adapters/BindingAdapters.kt
+++
@@ -1,9 +0,0 @@
-package com.deliveryhero.challenges.vendor.android.adapters
-
-import android.view.View
-import androidx.databinding.BindingAdapter
-
-@BindingAdapter("isGone")
-fun bindIsGone(view: View, isGone: Boolean) {
-    view.visibility = if (isGone) View.GONE else View.VISIBLE
-}
--- app/src/main/java/com/deliveryhero/challenges/vendor/android/utilities/Constants.kt
+++
@@ -1,7 +0,0 @@
-package com.deliveryhero.challenges.vendor.android.utilities
-
-/**
- * Constants used throughout the app.
- */
-const val DATABASE_NAME = "sunflower-db"
-const val PLANT_DATA_FILENAME = "plants.json"
